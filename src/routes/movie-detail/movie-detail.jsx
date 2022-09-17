193c437a74ec83d09e8f1a36abcbb47c9c9b710d
sagar-savvy <sagarsk1302@gmail.com>
2022-09-17 15:48:17 +0530

    styles for movie detail comp

--- src/routes/movie-detail/movie-detail.jsx
+++ src/routes/movie-detail/movie-detail.jsx
@@ -2,7 +2,7 @@ import { Link, useParams } from "react-router-dom"
 import { Routes, Route } from "react-router-dom";
 import styles from './styles.module.css'
 import Home from '../home/home'
-import {useState} from 'react'
+import { useState } from 'react'
 import { movie_list, theatre_list } from '../../structure'
 import MovieSelectOveraly from '../../components/movie-select-overlay/movie-select'
 
@@ -11,7 +11,7 @@ const MovieDetail = (props) => {
         <Routes>
             <Route index element={<Home />} />
             <Route path=":movieIndex" element={<MovieDeatiled />} />
-            <Route path=":movieIndex/booking/:language/:viewingType" element={<ViewTheatres/>} />
+            <Route path=":movieIndex/booking/:language/:viewingType" element={<ViewTheatres />} />
         </Routes>
     )
 }
@@ -19,39 +19,53 @@ const MovieDetail = (props) => {
 
 const MovieDeatiled = (props) => {
     const [openOverlay, setOpenOverlay] = useState(false)
-    const bookingHandler=() =>{
+    const bookingHandler = () => {
         setOpenOverlay(!openOverlay)
     }
     const { movieIndex } = useParams()
     const movie = movie_list[movieIndex]
-    const { banner_url, movie_name, genre, hearts, description } = movie
+    const { banner_url, movie_name, genre, hearts, description,certification,release_date,languages } = movie
     return (
         <div className={styles.tile}>
-            <img src={banner_url} alt={movie_name} className={styles.banner} />
-            <p className={styles.movie_name}>{movie_name}</p>
-            <p>{genre.map(genreIndividual => genreIndividual)}</p>
-            <p>{description}</p>
-            <p>{hearts}%</p>
-            <button onClick={bookingHandler}>Book Tickets</button>
-            {openOverlay?<MovieSelectOveraly movie={movie} setOpenOverlay={bookingHandler}/>:<></>}
+            <div className={styles.moviebanner}>
+                <img src={banner_url} alt={movie_name} className={styles.banner} />
+                <div className={styles.moviedetails}>
+                    <p className={styles.movie_name}>{movie_name}</p>
+                    <p>Genre: {genre.map(genreIndividual => genreIndividual+", ")}</p>
+                    <p>Likes: {hearts}%</p>
+                    <p>Certification: {certification}</p>
+                    <p>Release Date: {release_date}</p>
+                    <p>Description: {description}</p>
+                </div>
+            </div>
+            <button onClick={bookingHandler} className={styles.bookBtn}>Book Tickets</button>
+            {openOverlay ? <MovieSelectOveraly movie={movie} setOpenOverlay={bookingHandler} /> : <></>}
         </div>
     )
 }
 
-const ViewTheatres = ()=>{
-    const {movieIndex,language,viewingType} = useParams()
+const ViewTheatres = () => {
+    const { movieIndex, language, viewingType } = useParams()
     const movie = movie_list[movieIndex]
     const availabilityList = movie["availability"][language][0][viewingType]
     const [theatreList, setTheatreList] = useState(availabilityList[0].theatres)
     return (
         <div>
             {/* date selection */}
-            {theatreList.map(theatre=>{
+            {theatreList.map(theatre => {
                 return (
-                    <div className="card">
-                        <p>{theatre_list.find(theatre1=>theatre1["id"]==theatre["id"]).theatre_name}</p>
-                        <div className={styles.timings}>
-                        {theatre.timings.map((time,index)=>time)}
+                    <div className={styles.card}>
+                        <div className={styles.details}>
+                            <p>{theatre_list.find(theatre1 => theatre1["id"] == theatre["id"]).theatre_name}</p>
+                            <div className={styles.timings}>
+                                {theatre.timings.map((time, index) => {
+                                    return (
+                                        <div className={styles.time}>
+                                            {time}
+                                        </div>
+                                    )
+                                })}
+                            </div>
                         </div>
                     </div>
                 )
