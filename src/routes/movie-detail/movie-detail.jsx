16cd026003468148bf8764f3b604046e5b5d7a86
sagar-savvy <sagarsk1302@gmail.com>
2022-09-18 11:07:24 +0530

    select seats option added

--- src/routes/movie-detail/movie-detail.jsx
+++ src/routes/movie-detail/movie-detail.jsx
@@ -2,11 +2,17 @@ import { Link, useParams } from "react-router-dom"
 import { Routes, Route } from "react-router-dom";
 import styles from './styles.module.css'
 import Home from '../home/home'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { movie_list, theatre_list } from '../../structure'
 import MovieSelectOveraly from '../../components/movie-select-overlay/movie-select'
 import SelectPersonOverlay from '../../components/select-no-persons/selectPersons'
 import Overlay from '../../components/overlay/overlay'
+
+import { ReactComponent as Selected } from '../../assets/selected.svg'
+import { ReactComponent as Empty } from '../../assets/empty.svg'
+import { ReactComponent as Occupied } from '../../assets/occupied.svg'
+import { ReactComponent as CloseIcon } from '../../assets/close.svg'
+
 const MovieDetail = (props) => {
     return (
         <Routes>
@@ -25,14 +31,14 @@ const MovieDeatiled = (props) => {
     }
     const { movieIndex } = useParams()
     const movie = movie_list[movieIndex]
-    const { banner_url, movie_name, genre, hearts, description,certification,release_date,languages } = movie
+    const { banner_url, movie_name, genre, hearts, description, certification, release_date, languages } = movie
     return (
         <div className={styles.tile}>
             <div className={styles.moviebanner}>
                 <img src={banner_url} alt={movie_name} className={styles.banner} />
                 <div className={styles.moviedetails}>
                     <p className={styles.movie_name}>{movie_name}</p>
-                    <p>Genre: {genre.map(genreIndividual => genreIndividual+", ")}</p>
+                    <p>Genre: {genre.map(genreIndividual => genreIndividual + ", ")}</p>
                     <p>Likes: {hearts}%</p>
                     <p>Certification: {certification}</p>
                     <p>Release Date: {release_date}</p>
@@ -50,21 +56,30 @@ const ViewTheatres = () => {
     const movie = movie_list[movieIndex]
     const [selectedData, setSelectedData] = useState(null)
     const [selectSeatsFlag, setSelectSeatsFlag] = useState(false)
-    console.log(selectedData)
-    const [openSelectPersonOverlay,setOpenSelectPersonOverlay] = useState(false)
+    const [openSelectPersonOverlay, setOpenSelectPersonOverlay] = useState(false)
     const availabilityList = movie["availability"][language][0][viewingType]
     const [theatreList, setTheatreList] = useState(availabilityList[0].theatres)
-    const selectedTheatre = (theatre_id,time_index)=>{
-        setSelectedData({theatre_id,time_index})
+    const dateClickHandler = (index) => {
+        setTheatreList(availabilityList[index].theatres)
+    }
+    const selectedTheatre = (theatre_slot) => {
+        setSelectedData({ theatre_slot })
         toggleOverlay()
     }
-    const toggleOverlay=() =>{
+    const toggleOverlay = () => {
         setOpenSelectPersonOverlay(!openSelectPersonOverlay)
     }
     return (
         <div>
-            {/* date selection */}
-            {theatreList.map((theatre,index) => {
+            <div className={styles.dates}>
+                {availabilityList.map((avail, index) => {
+
+                    return (
+                        <p onClick={() => dateClickHandler(index)} className={styles.date} key={index}> {avail.date}</p>
+                    )
+                })}
+            </div>
+            {theatreList.map((theatre, index) => {
                 return (
                     <div className={styles.card} key={index}>
                         <div className={styles.details}>
@@ -72,7 +87,7 @@ const ViewTheatres = () => {
                             <div className={styles.timings}>
                                 {theatre.timings.map((time, index) => {
                                     return (
-                                        <div className={styles.time} onClick={()=>selectedTheatre(theatre["id"],index)} key={index}>
+                                        <div className={styles.time} onClick={() => selectedTheatre(theatre.slots[index])} key={index}>
                                             {time}
                                         </div>
                                     )
@@ -82,20 +97,45 @@ const ViewTheatres = () => {
                     </div>
                 )
             })}
-            {openSelectPersonOverlay?<SelectPersonOverlay onClick={toggleOverlay} setSelectedData={setSelectedData} setSelectSeatsFlag={setSelectSeatsFlag}/>:<></>}
-            {selectSeatsFlag?<SelectSeats data={selectedData} theatreList={theatreList}/>:<></>}
+            {openSelectPersonOverlay ? <SelectPersonOverlay onClick={toggleOverlay} setSelectedData={setSelectedData} setSelectSeatsFlag={setSelectSeatsFlag} /> : <></>}
+            {selectSeatsFlag ? <SelectSeats data={selectedData} theatreList={theatreList} setSelectSeatsFlag={setSelectSeatsFlag} /> : <></>}
         </div>
     )
 }
 
-const SelectSeats = (props)=>{
+const SelectSeats = (props) => {
     const { movieIndex, language, viewingType } = useParams()
-    const {theatre_id,time_index,number,theatreList} = props.data
+    const { theatre_slot, number, theatreList } = props.data
+    const [data, setData] = useState([])
+    const seatClickHandler = (event)=>{
+        
+    }
+    useEffect(() => {
+        const updateData = []
+        for (const property in theatre_slot) {
+            const row = theatre_slot[property]
+            console.log(row.seats);
+            const ele = (<div className={styles.row}>
+                <div>{property}</div>
+                <div className={styles.seats}>
+                    {row.seats.map(ele => ele ? <Empty className={styles.chair} onClick={seatClickHandler}/> : <Occupied className={styles.chair} />)}
+                </div>
+            </div>)
+            updateData.push(ele)
+        }
+        setData(updateData)
+        console.log(data);
+    }, [])
+    const closeClickHandler = () => {
+        props.setSelectSeatsFlag(false)
+    }
     return (
         <Overlay>
             <div className={styles.seatCard}>
-                sjdkadsklasd
-                askda;klsd
+                <p>No of people: {number}</p>
+                <p> Select Seats</p>
+                <CloseIcon className={styles.closeicon} onClick={closeClickHandler} />
+                {data.map(dat => dat)}
             </div>
         </Overlay>
     )
